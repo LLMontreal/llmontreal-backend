b15c6d60bb215b330b7456a9ef7eb6081d50031c
lucas.dantas <lucas.dantas@montreal.com.br>
2025-10-23 22:38:16 -0300

    first commit

---
+++ src/main/java/br/com/montreal/ai/llmontreal/LlmontrealApplication.java
@@ -0,0 +1,13 @@
+package br.com.montreal.ai.llmontreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmontrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmontrealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/montreal/ai/llmontreal/LlmontrealApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.montreal.ai.llmontreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmontrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
