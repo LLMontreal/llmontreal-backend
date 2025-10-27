b1077ca187a68417932395c03977db071163d0b1
lucas.dantas <lucas.mdantas@icloud.com>
2025-10-27 12:23:36 -0300

    refactor: use enum for status field

--- src/main/java/br/com/montreal/ai/llmontreal/entity/Document.java
+++ src/main/java/br/com/montreal/ai/llmontreal/entity/Document.java
@@ -1,5 +1,6 @@
 package br.com.montreal.ai.llmontreal.entity;
 
+import br.com.montreal.ai.llmontreal.entity.enums.DocumentStatus;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -19,9 +20,10 @@ public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private String status;
+    private DocumentStatus status;
     
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
