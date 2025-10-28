97790ac0cc8047f0742f9428965acdede58b7fb4
lucas.dantas <lucas.dantas@montreal.com.br>
2025-10-27 21:49:27 -0300

    refactor: use explicit constructor instead of @Autowired

--- src/main/java/br/com/montreal/ai/llmontreal/controller/DocumentController.java
+++ src/main/java/br/com/montreal/ai/llmontreal/controller/DocumentController.java
@@ -3,7 +3,6 @@ package br.com.montreal.ai.llmontreal.controller;
 import br.com.montreal.ai.llmontreal.dto.DocumentDTO;
 import br.com.montreal.ai.llmontreal.entity.enums.DocumentStatus;
 import br.com.montreal.ai.llmontreal.service.DocumentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -14,8 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/documents")
 public class DocumentController {
 
-    @Autowired
-    private DocumentService documentService;
+    private final DocumentService documentService;
+
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
 
     @GetMapping
     public ResponseEntity<Page<DocumentDTO>> getAllDocuments(
--- src/main/java/br/com/montreal/ai/llmontreal/service/DocumentService.java
+++ src/main/java/br/com/montreal/ai/llmontreal/service/DocumentService.java
@@ -3,7 +3,6 @@ package br.com.montreal.ai.llmontreal.service;
 import br.com.montreal.ai.llmontreal.entity.Document;
 import br.com.montreal.ai.llmontreal.entity.enums.DocumentStatus;
 import br.com.montreal.ai.llmontreal.repository.DocumentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,8 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class DocumentService {
 
-    @Autowired
-    private DocumentRepository documentRepository;
+    private final DocumentRepository documentRepository;
+
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
 
     public Page<Document> getAllDocuments(Pageable pageable, DocumentStatus documentStatus) {
         if (documentStatus == null) {
