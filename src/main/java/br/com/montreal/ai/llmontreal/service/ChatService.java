25da8b76d9721b99a84abeb2def384a5a5ccf2a9
lucas.dantas <lucas.dantas@montreal.com.br>
2025-10-31 16:51:58 -0300

    feat: add ChatService message functionality

---
+++ src/main/java/br/com/montreal/ai/llmontreal/service/ChatService.java
@@ -0,0 +1,92 @@
+package br.com.montreal.ai.llmontreal.service;
+
+import br.com.montreal.ai.llmontreal.dto.OllamaRequestDTO;
+import br.com.montreal.ai.llmontreal.dto.OllamaResponseDTO;
+import br.com.montreal.ai.llmontreal.entity.ChatMessage;
+import br.com.montreal.ai.llmontreal.entity.ChatSession;
+import br.com.montreal.ai.llmontreal.entity.Document;
+import br.com.montreal.ai.llmontreal.entity.enums.Author;
+import br.com.montreal.ai.llmontreal.exception.OllamaException;
+import br.com.montreal.ai.llmontreal.repository.ChatSessionRepository;
+import br.com.montreal.ai.llmontreal.repository.DocumentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Service
+public class ChatService {
+
+    private final ChatSessionRepository chatSessionRepository;
+    private final DocumentRepository documentRepository;
+    private final WebClient webClient;
+
+    private static final Logger log = LoggerFactory.getLogger(ChatService.class);
+
+    public ChatService(ChatSessionRepository chatSessionRepository, DocumentRepository documentRepository, WebClient webClient) {
+        this.chatSessionRepository = chatSessionRepository;
+        this.documentRepository = documentRepository;
+        this.webClient = webClient;
+    }
+
+    public Mono<OllamaResponseDTO> processMessage(OllamaRequestDTO requestDTO, Long chatSessionId, Long documentId) {
+        Document doc = documentRepository.findById(documentId)
+                .orElseThrow(() -> new EntityNotFoundException("Document not found by id: " + documentId));
+
+        ChatSession currentSession = chatSessionRepository.findById(chatSessionId)
+                .orElseGet(() -> createChatSession(requestDTO.model(), doc));
+
+        log.info("Calling {} for {} session...", requestDTO.model(), chatSessionId);
+
+        return webClient.post()
+                .uri("/api/generate")
+                .body(Mono.just(requestDTO), OllamaRequestDTO.class)
+                .retrieve()
+                .bodyToMono(OllamaResponseDTO.class)
+                .timeout(Duration.ofMinutes(2))
+                .doOnSuccess(res -> {
+                    log.info("Successfully received response from {}", requestDTO.model());
+                    addMessageToContext(chatSessionId, res.message(), res.author());
+                })
+                .onErrorMap(WebClientResponseException.class, ex -> {
+                    log.error("API call failed to Ollama model: {}. Status: {}, Response: {}",
+                            requestDTO.model(), ex.getStatusCode(), ex.getResponseBodyAsString());
+
+                    return new OllamaException("Error communicating with Ollama: " + ex.getResponseBodyAsString(), ex);
+                });
+    }
+
+    private ChatSession createChatSession(String model, Document doc) {
+        log.info("Creating chat session with {}...", model);
+
+        ChatSession cs = ChatSession.builder()
+                .createdAt(LocalDateTime.now())
+                .isActive(true)
+                .document(doc)
+                .build();
+
+        return chatSessionRepository.save(cs);
+    }
+
+    private void addMessageToContext(Long chatSessionId, String content, String author) {
+        ChatSession cs = chatSessionRepository.findById(chatSessionId)
+                .orElseThrow(() -> new EntityNotFoundException("Chat Session not found by id: " + chatSessionId));
+
+        ChatMessage chatMessage = ChatMessage.builder()
+                .author(Author.valueOf(author.toUpperCase()))
+                .createdAt(LocalDateTime.now())
+                .message(content)
+                .build();
+
+        cs.addMessage(chatMessage);
+        chatSessionRepository.save(cs);
+    }
+
+}
+
