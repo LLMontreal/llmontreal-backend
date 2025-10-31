5882b8e2d7962331d614faf37b2630efcf90e652
lucas.dantas <lucas.dantas@montreal.com.br>
2025-10-31 17:59:28 -0300

    fix: change 'message' property to 'response' to match ollama api

--- src/main/java/br/com/montreal/ai/llmontreal/dto/OllamaResponseDTO.java
+++ src/main/java/br/com/montreal/ai/llmontreal/dto/OllamaResponseDTO.java
@@ -2,5 +2,10 @@ package br.com.montreal.ai.llmontreal.dto;
 
 import java.time.LocalDateTime;
 
-public record OllamaResponseDTO(Long chatSessionId, Long id, String author, LocalDateTime createdAt, String message) {
+public record OllamaResponseDTO(
+        Long chatSessionId,
+        Long id,
+        String author,
+        LocalDateTime createdAt,
+        String response) {
 }
