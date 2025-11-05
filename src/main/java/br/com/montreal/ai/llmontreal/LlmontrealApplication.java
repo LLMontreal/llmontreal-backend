8a085f8943d5e22c92ecbd11dc6b14827bc03c5f
ro77en <lucas.mdantas@icloud.com>
2025-11-04 22:41:51 -0300

    feat: enable scheduling

--- src/main/java/br/com/montreal/ai/llmontreal/LlmontrealApplication.java
+++ src/main/java/br/com/montreal/ai/llmontreal/LlmontrealApplication.java
@@ -3,9 +3,11 @@ package br.com.montreal.ai.llmontreal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class LlmontrealApplication {
 
 	public static void main(String[] args) {
