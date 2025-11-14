cfbb8f40ff1b2f71be63a8721c97daebecb37212
ro77en <lucas.mdantas@icloud.com>
2025-11-13 22:34:10 -0300

    refactor: update imports

--- src/main/java/br/com/montreal/ai/llmontreal/controller/ChatController.java
+++ src/main/java/br/com/montreal/ai/llmontreal/controller/ChatController.java
@@ -1,8 +1,8 @@
 package br.com.montreal.ai.llmontreal.controller;
 
-import br.com.montreal.ai.llmontreal.dto.ChatMessageRequestDTO;
+import br.com.montreal.ai.llmontreal.dto.OllamaRequestDTO;
 import br.com.montreal.ai.llmontreal.dto.ChatMessageResponseDTO;
-import br.com.montreal.ai.llmontreal.service.ChatProducerService;
+import br.com.montreal.ai.llmontreal.service.ollama.OllamaProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -15,14 +15,14 @@ import java.util.concurrent.CompletableFuture;
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final ChatProducerService chatProducerService;
+    private final OllamaProducerService ollamaProducerService;
 
     @PostMapping("/{documentId}")
     public Mono<ChatMessageResponseDTO> sendMessageToOllama(
-            @RequestBody @Valid ChatMessageRequestDTO requestDTO,
+            @RequestBody @Valid OllamaRequestDTO requestDTO,
             @PathVariable Long documentId
     ) {
-        CompletableFuture<ChatMessageResponseDTO> responseFuture = chatProducerService.processMessage(requestDTO, documentId);
+        CompletableFuture<ChatMessageResponseDTO> responseFuture = ollamaProducerService.processMessage(requestDTO, documentId);
         return Mono.fromFuture(responseFuture);
     }
 }
